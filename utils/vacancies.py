88c418908608b8e5f4b5c63257d01182770b2759
igorpetushkov <igor.igorevich08@gmail.com>
2023-08-01 16:56:02 +0300

    vacancy class

---
+++ utils/vacancies.py
@@ -0,0 +1,7 @@
+class Vacancy:
+    def __init__(self, title, city, requirement, url, payment):
+        self.title = title
+        self.city = city
+        self.requirement = requirement
+        self.url = url
+        self.payment = payment
